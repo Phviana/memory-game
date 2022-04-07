459bac68d027010bb8c64d0b8fdf585f7ed1952c
Paulo Viana <paulohenrique_viana@hotmail.com>
2022-04-07 22:52:37 +0100

    game completed :video_game:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { SingleCard } from './components/SingleCard';
 import './App.css';
 
+const cardImages = [
+  { "src": "/img/helmet-1.png" , matched: false},
+  { "src": "/img/potion-1.png" , matched: false},
+  { "src": "/img/ring-1.png" , matched: false},
+  { "src": "/img/scroll-1.png" , matched: false},
+  { "src": "/img/shield-1.png" , matched: false},
+  { "src": "/img/sword-1.png" , matched: false},
+]
+
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
+
+  function shuffleCards() {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+
+    setCards(shuffledCards)
+    setTurns(0)
+    setChoiceOne(null)
+    setChoiceTwo(null)
+  }
+
+  function resetTurns() {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurn => prevTurn + 1)
+    setDisabled(false)
+  }
+
+  function handleChoice(card) {
+    choiceOne === null ? setChoiceOne(card) : setChoiceTwo(card)
+  }
+
+  // starts the game automatically 
+  useEffect(() =>{
+    shuffleCards()
+  },[])
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true)
+
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurns()
+      } else {
+        setTimeout(() => resetTurns(), 1000)
+      }
+
+    }
+  }, [choiceOne, choiceTwo])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className='card-grid'>
+        {cards.map(card => (
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
